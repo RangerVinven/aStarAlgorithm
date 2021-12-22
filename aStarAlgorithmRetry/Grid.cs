fe9d5541266169f4889e49003b1eb7a7af0804ad
RangerVinven <daniel.mcpherson@live.co.uk>
2021-12-22 19:33:29 +0000

    Made the findTouchingNodes method and changed the accessibility of the variables in the Node class

--- aStarAlgorithmRetry/Grid.cs
+++ aStarAlgorithmRetry/Grid.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Transactions;
 
 namespace aStarAlgorithmRetry
 {
@@ -8,6 +10,7 @@ namespace aStarAlgorithmRetry
         private Node currentNode { get; set; }
         private Node startNode { get; set; }
         private Node endNode { get; set; }
+        private int distanceTravelled { get; set; }
 
         // For keeping track of which nodes to consider when choosing the next node
         private List<Tuple<int, Tuple<int, int>>> nodesToConsider { get; set; }
@@ -18,7 +21,85 @@ namespace aStarAlgorithmRetry
             currentNode = startNode;
         }
         
-        public static Grid findTouchingNodes() {}
+        public static Grid findTouchingNodes(Grid grid) {
+            Node touchingNode;
+            
+            // Checks if the current node is touching the above node (or if it's at the top) and that it isn't an obstacle
+            if (grid.currentNode.coords.Item1 > 0) {
+                // Makes sure the above node isn't an obstacle
+                if (grid.grid[grid.currentNode.coords.Item1 - 1][grid.currentNode.coords.Item2].type != Node.Type.Obstacle) {
+
+                    // Sets the touchingNode variable
+                    touchingNode = new Node(new Tuple<int, int>(grid.currentNode.coords.Item1 - 1, grid.currentNode.coords.Item2), Node.Type.Regular);
+
+                    // Gets the h, g and f cost of the touching node 
+                    int hCostOfTouchingNode = Node.calculateHcost(new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2), new Tuple<int, int>(grid.endNode.coords.Item1, grid.endNode.coords.Item2));
+                    int gCostOfTouchingNode = Node.calculateGcost(grid.distanceTravelled);
+                    int fCostOfTouchingNode = Node.calculateFcost(hCostOfTouchingNode, gCostOfTouchingNode);
+                    
+                    // Adds the touching node to the nodesToConsider list
+                    grid.nodesToConsider.Add(new Tuple<int, Tuple<int, int>>(fCostOfTouchingNode, new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2)));
+                }
+
+            }
+            
+            // Checks if the current node is touching the node to the right and that it isn't an obstacle
+            if (grid.currentNode.coords.Item2 < grid.grid[0].Count) {
+                // Makes sure the above node isn't an obstacle
+                if (grid.grid[grid.currentNode.coords.Item1][grid.currentNode.coords.Item2 + 1].type != Node.Type.Obstacle) {
+
+                    // Sets the touchingNode variable
+                    touchingNode = new Node(new Tuple<int, int>(grid.currentNode.coords.Item1, grid.currentNode.coords.Item2 + 1), Node.Type.Regular);
+
+                    // Gets the h, g and f cost of the touching node 
+                    int hCostOfTouchingNode = Node.calculateHcost(new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2), new Tuple<int, int>(grid.endNode.coords.Item1, grid.endNode.coords.Item2));
+                    int gCostOfTouchingNode = Node.calculateGcost(grid.distanceTravelled);
+                    int fCostOfTouchingNode = Node.calculateFcost(hCostOfTouchingNode, gCostOfTouchingNode);
+                    
+                    // Adds the touching node to the nodesToConsider list
+                    grid.nodesToConsider.Add(new Tuple<int, Tuple<int, int>>(fCostOfTouchingNode, new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2)));
+                }
+
+            }
+            
+            // Checks if the current node is touching the below node and that it isn't an obstacle
+            if (grid.currentNode.coords.Item1 < grid.grid.Count) {
+                // Makes sure the above node isn't an obstacle
+                if (grid.grid[grid.currentNode.coords.Item1 + 1][grid.currentNode.coords.Item2].type != Node.Type.Obstacle) {
+
+                    // Sets the touchingNode variable
+                    touchingNode = new Node(new Tuple<int, int>(grid.currentNode.coords.Item1 + 1, grid.currentNode.coords.Item2), Node.Type.Regular);
+
+                    // Gets the h, g and f cost of the touching node 
+                    int hCostOfTouchingNode = Node.calculateHcost(new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2), new Tuple<int, int>(grid.endNode.coords.Item1, grid.endNode.coords.Item2));
+                    int gCostOfTouchingNode = Node.calculateGcost(grid.distanceTravelled);
+                    int fCostOfTouchingNode = Node.calculateFcost(hCostOfTouchingNode, gCostOfTouchingNode);
+                    
+                    // Adds the touching node to the nodesToConsider list
+                    grid.nodesToConsider.Add(new Tuple<int, Tuple<int, int>>(fCostOfTouchingNode, new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2)));
+                }
+
+            }
+            
+            // Checks if the current node is touching the node to the left and that it isn't an obstacle
+            if (grid.currentNode.coords.Item2 > 0) {
+                // Makes sure the above node isn't an obstacle
+                if (grid.grid[grid.currentNode.coords.Item1][grid.currentNode.coords.Item2 - 1].type != Node.Type.Obstacle) {
+
+                    // Sets the touchingNode variable
+                    touchingNode = new Node(new Tuple<int, int>(grid.currentNode.coords.Item1, grid.currentNode.coords.Item2 - 1), Node.Type.Regular);
+
+                    // Gets the h, g and f cost of the touching node 
+                    int hCostOfTouchingNode = Node.calculateHcost(new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2), new Tuple<int, int>(grid.endNode.coords.Item1, grid.endNode.coords.Item2));
+                    int gCostOfTouchingNode = Node.calculateGcost(grid.distanceTravelled);
+                    int fCostOfTouchingNode = Node.calculateFcost(hCostOfTouchingNode, gCostOfTouchingNode);
+                    
+                    // Adds the touching node to the nodesToConsider list
+                    grid.nodesToConsider.Add(new Tuple<int, Tuple<int, int>>(fCostOfTouchingNode, new Tuple<int, int>(touchingNode.coords.Item1, touchingNode.coords.Item2)));
+                }
+
+            }
+        }
         
         public static Node findCostsOfNode(Node node) {}
         
--- aStarAlgorithmRetry/Node.cs
+++ aStarAlgorithmRetry/Node.cs
@@ -11,12 +11,12 @@ namespace aStarAlgorithmRetry
             Regular
         }
         
-        private int? hCost { get; set; }
+        public int? hCost { get; set; }
         private int? gCost { get; set; }
-        private int? fCost { get; set; }
-        private Tuple<int, int> coords;
-        private Type type;
-        private List<Tuple<int, int>> touchingNodesCoords;
+        public int? fCost { get; set; }
+        public Tuple<int, int> coords;
+        public Type type;
+        public List<Tuple<int, int>> touchingNodesCoords;
 
         public Node(Tuple<int, int> nodeCoords, Type nodeType) {
             coords = nodeCoords;
@@ -29,17 +29,17 @@ namespace aStarAlgorithmRetry
         }
 
         // Calculates the h cost of the node
-        public int calculateHcost(Tuple<int, int> nodeCoords, Tuple<int, int> endNodeCoords) {
+        public static int calculateHcost(Tuple<int, int> nodeCoords, Tuple<int, int> endNodeCoords) {
             return Math.Abs(nodeCoords.Item1 - endNodeCoords.Item1) + Math.Abs(nodeCoords.Item2 - endNodeCoords.Item2);
         }
 
         // Calculate the g cost of the node
-        public int calculateGcost(int distanceTravelled) {
+        public static int calculateGcost(int distanceTravelled) {
             return distanceTravelled + 1;
         }
 
         // Calculate the f cost of the node
-        public int calculateFcost(int hCost, int gCost) {
+        public static int calculateFcost(int hCost, int gCost) {
             return hCost + gCost;
         }
 
